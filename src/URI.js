7e643be805f3a0718b9652b30d8b2ceffe205a8c
Rafa Serna <rafarks@gmail.com>
2024-08-24 15:57:42 -0500

    changed copy api to use the Clipborad API

--- src/URI.js
+++ src/URI.js
@@ -2,22 +2,22 @@ import { useSelector } from "react-redux";
 import { urls } from "./CouponURLs";
 import QueryParameters from "./QueryParameters";
 import URIPath from "./URIPath";
-
 import URITypeMenu from "./URITypeMenu";
-import ClipboardJS from 'clipboard';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Enabled from "./Enabled";
+import classNames from "classnames";
 
 const URI = () => {
     const queryParameters = useSelector(state => state.queryParameters)
     const uri = useSelector(state => state.uri)
     const fullURL = `${urls.homepage}${uri.type === 'path'? '/'+uri.value:''}${stringifyQueryParameters(queryParameters)}`
+    const [isCopying, setIsCopying] = useState(false)
 
     useEffect(() => {
-        new ClipboardJS('#coupon–urls–copy–button', {
-            text: () => fullURL
-        });
-    }, [fullURL])
+        if (isCopying) {
+            setTimeout(() => setIsCopying(false), 4*100)
+        }
+    }, [isCopying])
 
     return <div className="relative flex flex-row justify-center">
                 <div className="flex flex-row relative rounded-3 bg-gray-100 p-3 shadow-card mb-6 z-10">
@@ -35,11 +35,32 @@ const URI = () => {
                         <button 
                             id="coupon–urls–copy–button" 
                             className="px-2 bg-gray-350 text-gray-100 rounded-6 uppercase text-smaller-2 group"
+                            onClick={async () => {
+                                try {
+                                    await navigator.clipboard.writeText(fullURL)
+                                } catch(error) {
+                                    console.log('error copying url:', error)
+                                }
 
+                                setIsCopying(true)
+                            }}
                             >
-                            <span className="group-focus:hidden">{__('copy')}</span>
-                            <span className="hidden group-focus:inline">{__('Copied!')}</span>
+                            <span className={classNames({
+                                'hidden': isCopying
+                            })}>{__('copy')}</span>
+                            <span className={classNames({
+                                'hidden': !isCopying
+                            })}>{__('Copied!')}</span>
                         </button>
+                        <a  href={fullURL}
+                            target="_blank"
+                            rel="noreferrer"
+                            className="flex items-center space-x-1 px-2 bg-[#6c90e0] text-gray-100 rounded-6 uppercase text-smaller-2 group h-4"
+                            >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="min-w-4 max-w-4 min-h-4 max-h-4">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
+                            </svg>
+                        </a>
                     </p>
                 </div>
                 <Enabled />
